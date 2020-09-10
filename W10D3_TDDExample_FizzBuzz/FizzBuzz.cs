70c6918749122a9153302c1ad2627a488fcfe7e1
hankins0202 <waltone87@gmail.com>
2020-09-10 19:41:33 -0400

    Added TDD Fizz Buzz first passing test

---
+++ W10D3_TDDExample_FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace W10D3_TDDExample_FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        public string Generate(int input)
+        {
+            return "Fizz";
+        }
+    }
+}
---
+++ W10D3_TDDExample_FizzBuzzTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using W10D3_TDDExample_FizzBuzz;
+using Xunit;
+
+namespace W10D3_TDDExample_FizzBuzzTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ShouldReturnFizzForMultipleOFThree()
+        {
+            //Arrange
+            var input = 3;
+            var expectedResult = "Fizz";
+            FizzBuzz fizzBuzz = new FizzBuzz();
+
+            //Act
+            string result = fizzBuzz.Generate(input);
+
+            //Assert
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
