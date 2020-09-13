9ec065ef7fbafbf9810b00d1cb98842f238aec27
hankins0202 <waltone87@gmail.com>
2020-09-13 13:44:01 -0400

    Refactored using switch case, prior was if statements.  Removed inconclusive enum (not needed anymore), 6 passing tests

--- ROSHAMBO/InputService.cs
+++ ROSHAMBO/InputService.cs
@@ -6,36 +6,42 @@ namespace ROSHAMBO
 
         public RPS Play(RPS input, RPS input1)
         {
-            //switch (input)
-            //{
-            //    case RPS.Paper:
-            //        if (input1.Equals(RPS.Rock))
-            //            return RPS.Paper;
-            //        else
-            //            return RPS.Scissors;
-            //    case RPS.Rock:
-            //        if (input1.Equals(RPS.Scissors))
-            //            return RPS.Rock;
-            //        else
-            //            return RPS.Paper;
+            RPS result = new RPS();
+            switch (input)
+            {
+                case RPS.Paper:
+                    if (input1.Equals(RPS.Rock))
+                        result = RPS.Paper;
+                    else
+                        result =  RPS.Scissors;
+                    break;
+                case RPS.Rock:
+                    if (input1.Equals(RPS.Scissors))
+                        result =  RPS.Rock;
+                    else
+                        result =  RPS.Paper;
+                    break;
+                case RPS.Scissors:
+                    if (input1.Equals(RPS.Paper))
+                        result =  RPS.Scissors;
+                    else
+                        result =  RPS.Rock;
+                    break;
+            }
+            return result;
 
-
-
-            //}
-                
-
-            if (input.Equals(RPS.Paper) && input1.Equals(RPS.Rock))
-                return RPS.Paper;
-            else if (input.Equals(RPS.Rock) && input1.Equals(RPS.Scissors))
-                return RPS.Rock;
-            else if (input.Equals(RPS.Scissors) && input1.Equals(RPS.Paper))
-                return RPS.Scissors;
-            else if (input.Equals(RPS.Rock) && input1.Equals(RPS.Paper))
-                return RPS.Paper;
-            else if (input.Equals(RPS.Scissors) && input1.Equals(RPS.Rock))
-                return RPS.Rock;
-            else
-                return RPS.Inconclusive;
+            //if (input.Equals(RPS.Paper) && input1.Equals(RPS.Rock))
+            //    return RPS.Paper;
+            //else if (input.Equals(RPS.Rock) && input1.Equals(RPS.Scissors))
+            //    return RPS.Rock;
+            //else if (input.Equals(RPS.Scissors) && input1.Equals(RPS.Paper))
+            //    return RPS.Scissors;
+            //else if (input.Equals(RPS.Rock) && input1.Equals(RPS.Paper))
+            //    return RPS.Paper;
+            //else if (input.Equals(RPS.Scissors) && input1.Equals(RPS.Rock))
+            //    return RPS.Rock;
+            //else
+            //    return RPS.Inconclusive;
         }
     }
 }
