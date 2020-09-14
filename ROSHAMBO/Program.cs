6354864a04732cd4a7b17145783722e6b9626daa
hankins0202 <waltone87@gmail.com>
2020-09-13 22:21:10 -0400

    Refactored program. Final commit for 9/13/2020

--- ROSHAMBO/Program.cs
+++ ROSHAMBO/Program.cs
@@ -6,68 +6,72 @@ namespace ROSHAMBO
     {
         static void Main(string[] args)
         {
-        //    Console.WriteLine("Welcome to ROSHAMBO");
-        //    do
-        //    { 
-                
-               
-        //        Console.WriteLine("First player, choose your weapon of mass destruction. 0. Rock, 1. Paper, or 2. Scissors.");
-        //        var input = Console.ReadLine();
-        //        if (!int.TryParse(input, out int ValidInput)
-        //        {
-        //            Console.Clear();
-        //            Console.WriteLine("Invalid Entry.Choose 0.Rock, 1.Paper, or 2.Scissors.");
+            InputService inputService = new InputService();
+            Console.WriteLine("Welcome to ROSHAMBO");
+            do
+            {
+                bool proceed = false;
+                string input = "";
+                string input1 = "";
+                do
+                {
+                    Console.WriteLine("Player 1 choose your weapon. 0. Rock, 1. Paper, or 2. Scissors");
+                    input = Console.ReadLine();
+                    if(inputService.ValidateUserInput(input))
+                    {
+                        Console.Clear();
+                        do
+                        {
+                            Console.WriteLine("Player 2 choose your weapon. 0. Rock, 1. Paper, or 2. Scissors");
+                            input1 = Console.ReadLine();
+                            if (inputService.ValidateUserInput(input1))
+                            {
+                                RPS play = inputService.Play((RPS)int.Parse(input), (RPS)int.Parse(input1));
+                                var result = play == RPS.Tie ? "Tie game!" : $"{play} wins!";
+                                Console.WriteLine(result);
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid Entry.");
+                                proceed = false;
+                                continue;
+                            }
 
-        //        }
-                
-        //        Console.WriteLine("Second player, choose your weapon of mass destruction. 0. Rock, 1. Paper, or 2. Scissors.");
-        //        int input1 = int.Parse(Console.ReadLine());
-        //        while (input <= 2 && input1 <= 2)
-        //        {
-        //            InputService inputService = new InputService();
-        //            var Play = inputService.Play((RPS)Convert.ToInt32(input), (RPS)Convert.ToInt32(input1));
-        //            var result = Play == RPS.Tie ? "Tie game!" : $"{Play} wins!";
-        //            Console.WriteLine(result);
-        //        }
-        //        Console.WriteLine("Play again? Y for yes or N to quit.");
+                        } while (proceed == false);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Entry.");
+                        proceed = false;
+                        continue;
+                    }
+                } while (proceed == false);
 
-        //    } while (UserContinue());
-        //}
-        //public static bool UserContinue()
-        //{
-        //    while (true)
-        //    {
-        //        var input = Console.ReadLine().ToUpper();
-        //        if (input == "Y")
-        //        {
-        //            Console.Clear();
-        //            return true;
-        //        }
-        //        else if (input == "N")
-        //        {
-        //            return false;
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("You did not enter  'Y' or 'N' ");
-        //        }
-        //    }
-        //}
-        //public static int PlayProceedInputValidator()
-        //{
-           
-        //    bool Valid = false;
-        //    string input = Console.ReadLine();
-        //    while (!Valid)
-        //    {
-        //        Valid = !string.IsNullOrWhiteSpace(input) && int.Parse(input) <= 2;
-        //        if(!Valid)
-        //        {
-        //            Console.WriteLine("Invalid Entry. Choose 0. Rock, 1. Paper, or 2. Scissors.");
-        //        }
-        //    }
-        //    return int.Parse(input);
-        //
+                Console.WriteLine("Play again? Y for yes or N to quit.");
+
+            } while (UserContinue());
+        }
+        public static bool UserContinue()
+        {
+            while (true)
+            {
+                var input = Console.ReadLine().ToUpper();
+                if (input == "Y")
+                {
+                    Console.Clear();
+                    return true;
+                }
+                else if (input == "N")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter  'Y' or 'N' ");
+                }
+            }
         }
     }
 }
