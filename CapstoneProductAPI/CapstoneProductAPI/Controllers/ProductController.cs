210bd1029f874821a787a9beb24231825f8b59af
Elizabeth Hankins <elizabethhankins@Elizabeths-MacBook-Pro.local>
2020-11-12 18:26:47 -0500

    Capstone Product API Initial commit

---
+++ CapstoneProductAPI/CapstoneProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CapstoneProductAPI.APIContext;
+using CapstoneProductAPI.Models;
+
+namespace CapstoneProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public ProductController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Product
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+        // GET: api/Product/Category/id
+        [HttpGet("Category/{id}")]
+        public async Task<ActionResult<List<Product>>> GetProductByCategory(int id)
+        {
+            var products = await _context.Products.Where(p => p.CategoryId == id).ToListAsync();
+
+            if (products == null)
+            {
+                return NotFound();
+            }
+
+            return products;
+        }
+
+        // PUT: api/Product/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Product
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Product/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
---
+++ CapstoneProductAPI/CapstoneProductAPI/Controllers/SupplierController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CapstoneProductAPI.APIContext;
+using CapstoneProductAPI.Models;
+
+namespace CapstoneProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SupplierController : ControllerBase
+    {
+        private readonly NorthwindContext _context;
+
+        public SupplierController(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Supplier
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+
+        // GET: api/Supplier/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Supplier>> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return supplier;
+        }
+
+        // PUT: api/Supplier/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSupplier(int id, Supplier supplier)
+        {
+            if (id != supplier.SupplierId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(supplier).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SupplierExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Supplier
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Supplier>> PostSupplier(Supplier supplier)
+        {
+            _context.Suppliers.Add(supplier);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSupplier", new { id = supplier.SupplierId }, supplier);
+        }
+
+        // DELETE: api/Supplier/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Supplier>> DeleteSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            _context.Suppliers.Remove(supplier);
+            await _context.SaveChangesAsync();
+
+            return supplier;
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return _context.Suppliers.Any(e => e.SupplierId == id);
+        }
+    }
+}
