c55e8fed91cffe1b17c669fa80f34a0c2aff6492
Elizabeth Hankins <elizabethhankins@Elizabeths-MacBook-Pro.local>
2020-11-18 17:25:56 -0500

    MockAssessment7 initial commit

---
+++ MockAssessment7/MockAssessment7/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MockAssessment7.Models;
+
+namespace MockAssessment7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://grandcircusco.github.io/demo-apis/donuts")
+            };
+
+            _httpClient = httpClient;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Get: Products/3.json
+        public IActionResult Search(int id)
+        {
+
+            var response = _httpClient.GetAsync($"/demo-apis/donuts/{id}.json").GetAwaiter().GetResult();
+            var donutjson = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var donut = JsonSerializer.Deserialize<Donut>(donutjson, options);
+            donut.PhotoURL = $"https://grandcircusco.github.io/demo-apis/donuts/images/d{id}.jpg";
+            return View(donut);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MockAssessment7/MockAssessment7/Models/Donut.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MockAssessment7.Models
+{
+    public class Donut
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public string PhotoURL { get; set; }
+        public string [] Extras { get; set; }
+    }
+}
